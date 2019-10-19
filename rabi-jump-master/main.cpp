08763c745d44bd152557e0ea8bf782c7d410d1aa
司马坑 <simakeng@outlook.com>
2019-10-19 22:43:27 +0800

    add items

---
+++ rabi-jump-master/main.cpp
@@ -0,0 +1,72 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <WinSock2.h>
+#include <cstdio>
+#include <cstdint>
+#pragma comment(lib,"ws2_32.lib")
+
+#include "..//Shared/Common.Datas.h"
+int main(int argc, char* args)
+{
+	WORD sockVersion = MAKEWORD(2, 2);
+	WSADATA wsaData;
+	if (WSAStartup(sockVersion, &wsaData) != 0)
+	{
+		return 0;
+	}
+
+	SOCKET slisten = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (slisten == INVALID_SOCKET)
+	{
+		printf("socket error !");
+		return 0;
+	}
+
+
+	sockaddr_in sin;
+	sin.sin_family = AF_INET;
+	sin.sin_port = htons(8085);
+	sin.sin_addr.S_un.S_addr = INADDR_ANY;
+	if (bind(slisten, (LPSOCKADDR)& sin, sizeof(sin)) == SOCKET_ERROR)
+	{
+		printf("bind error !");
+	}
+
+	//开始监听
+	if (listen(slisten, 5) == SOCKET_ERROR)
+	{
+		printf("listen error !");
+		return 0;
+	}
+
+
+	SOCKET sClient;
+	sockaddr_in remoteAddr;
+	int nAddrlen = sizeof(remoteAddr);
+	char revData[255];
+	while (true)
+	{
+		printf("等待连接...\n");
+		sClient = accept(slisten, (SOCKADDR*)& remoteAddr, &nAddrlen);
+		if (sClient == INVALID_SOCKET)
+		{
+			printf("accept error !");
+			continue;
+		}
+		printf("接受到一个连接：%s \r\n", inet_ntoa(remoteAddr.sin_addr));
+
+		//接收数据
+		int ret = recv(sClient, revData, 255, 0);
+		if (ret > 0)
+		{
+			revData[ret] = 0x00;
+			printf(revData);
+		}
+
+		//发送数据
+		const char* sendData = "你好，TCP客户端！\n";
+		send(sClient, sendData, strlen(sendData), 0);
+		closesocket(sClient);
+	}
+
+	return 0;
+}
